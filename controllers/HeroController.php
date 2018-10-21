c5ee5fa2602a196869472183e28b934b0e327bf6
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2018-10-21 19:46:54 +0700

    Allow cors from all

--- controllers/HeroController.php
+++ controllers/HeroController.php
@@ -17,16 +17,16 @@ class HeroController extends ActiveController
         // add CORS filter
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::className(),
-            // 'cors'  => [
-            //     // restrict access to domains:
-            //     'Origin'                           => [
-            //         'http://localhost:4200'
-            //         // '*'
-            //     ],
-            //     'Access-Control-Request-Method'    => ['POST'],
-            //     'Access-Control-Allow-Credentials' => true,
-            //     'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
-            // ],
+            'cors'  => [
+                // restrict access to domains:
+                'Origin'                           => [
+                    // 'http://localhost:4200'
+                    '*'
+                ],
+                'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE', 'VIEW'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
+            ],
         ];
         
         return $behaviors;
